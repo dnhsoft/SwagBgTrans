2576ca97eeb9a8f0fabb75c606f22b3091d191ab
Stefan Bofirov <bofirov@gmail.com>
2014-12-23 13:33:52 +0200

    Frontend translations

---
+++ Bootstrap.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Class Shopware_Plugins_Frontend_SwagBgTrans_Bootstrap
+ *
+ * @package Shopware\Plugins\Local\SwagBgTrans
+ * @author Stefan Bofirov
+ */
+class Shopware_Plugins_Frontend_SwagBgTrans_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+    /**
+     * Returns current plugin version
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return '0.0.1';
+        var_dump('sb:da');exit;
+    }
+
+    /**
+     * Returns a nice name for plugin manager list
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return 'Bulgarian Translation';
+    }
+
+    /**
+     * Returns plugin information for the plugin manager
+     *
+     * @return array
+     */
+    public function getInfo()
+    {
+        return array(
+            'version' => $this->getVersion(),
+            'label' => $this->getLabel(),
+            'description' => $this->getDescription(),
+        );
+    }
+
+    /**
+     * Returns plugin description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return 'Lorem ipsum';
+    }
+
+    /**
+     * Install plugin method
+     *
+     * @return array|bool
+     */
+    public function install()
+    {
+        $this->importFrontendTranslations();
+        $this->importFrontendTranslations();
+
+        return array('success' => true, 'invalidateCache' => array('backend', 'config'));
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function uninstall()
+    {
+        $sql = "DELETE FROM `s_core_snippets` WHERE `localeID` = 30;";
+        Shopware()->Db()->exec($sql);
+
+        return true;
+    }
+
+    public function importFrontendTranslations()
+    {
+        $sql = file_get_contents($this->Path() . 'Snippets/frontend.sql');
+        Shopware()->Db()->exec($sql);
+    }
+
+    public function importBackendTranslations()
+    {
+
+    }
+} 
